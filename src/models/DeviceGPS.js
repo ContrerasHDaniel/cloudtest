9254a978e0ddd5bb00b51f1eb0aa89517b8fed1b
ContrerasHDaniel <contrerashdaniel@hotmail.com>
2019-08-19 15:25:21 -0500

    La posicion de los dispositivos ahora se identifica por la hora de llegada al server. Se espera que sea mas simple obtener la ultima posicion de este modo

--- src/models/DeviceGPS.js
+++ src/models/DeviceGPS.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const {Schema} = mongoose;
 
-const positionSchema = new Schema({lat: String, lng: String});
+const positionSchema = new Schema({_id: Date, lat: String, lng: String});
 const DeviceGPSSchema = new Schema({
     _id: String,
     name: String,
--- src/routes/dgps.js
+++ src/routes/dgps.js
@@ -5,11 +5,11 @@ const DeviceGPS = require('../models/DeviceGPS');
 router.post('/devices/dgps', async (req, res) => {    
     const {_id, name, group_id, lat, lng } = req.body;
     try{
-        const newReg = new DeviceGPS({_id:_id, name:name, group_id:group_id, position:{lat:lat,lng:lng}});
+        const newReg = new DeviceGPS({_id:_id, name:name, group_id:group_id, position:{_id: Date.now(), lat:lat,lng:lng}});
         await newReg.save();
         res.sendStatus(201);
     }catch(error){
-        const device = await DeviceGPS.findByIdAndUpdate(_id, {$push: {position:{lat:lat, lng:lng}}});
+        const device = await DeviceGPS.findByIdAndUpdate(_id, {$push: {position:{_id: Date.now(), lat:lat, lng:lng}}});
         console.log('update');
         res.sendStatus(200);
     }
