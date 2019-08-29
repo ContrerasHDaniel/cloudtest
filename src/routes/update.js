2b1db7a63d5bbf64a8b92534da7a8d3525e96139
Daniel Contreras <contrerashdaniel@hotmail.com>
2019-08-29 01:41:55 -0500

    Modificado frontend update

--- src/routes/update.js
+++ src/routes/update.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const DeviceGPS = require('../models/DeviceGPS');
+const ZonaSchema = require('../models/Zona');
 
-router.get('/update',(req, res) => {
-	  res.render('update');
+router.get('/update', async (req, res) => {
+	const zonas = await ZonaSchema.find();
+	const devices = await DeviceGPS.find();
+	res.render('update', {zonas, devices});
 });
 
 module.exports = router;
